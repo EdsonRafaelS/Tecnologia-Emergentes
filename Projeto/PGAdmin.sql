2bc186abef1ba4696bff4152185460dc86db76d0
Edson Rafael <130865402+EdsonRafaelS@users.noreply.github.com>
2023-11-28 15:40:42 -0300

    projeto_ii

--- Projeto/PGAdmin.sql
+++ Projeto/PGAdmin.sql
@@ -14,9 +14,30 @@ create table semestre(
 );
 
 create table oferta(
-    id int primary key,
+    id_oferta int primary key,
     ano int,
     semestre int,
     id_disciplina int references disciplina(id),
     foreign key (ano, semestre) references semestre(ano, semestre)
+);
+
+create table datas_importantes(
+	id int primary key,
+	data_importante date not null,
+	descricao_data varchar(30) not null,
+	tipo_data varchar(30) not null
+);
+
+create table cronograma(
+	id_cronograma int primary key,
+	data_inicio_semestre date not null,
+	data_termino_semestre date not null,
+	dias_de_aulas varchar[],
+	id_oferta int references oferta(id_oferta)
+);
+
+create table conteudos(
+	id int primary key, 
+	id_cronograma int references cronograma(id_cronograma),
+	quantidade_aulas int not null
 );
